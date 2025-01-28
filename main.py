878216e0265a2f81435399bbde2c51ec0ef63268
DevPoncet07 <>
2025-01-28 20:04:18 +0100

    Last Sync: 2025-01-28 19:04:18 (Mobile)

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from tkinter import *
+from tkinter import Tk
+
+from src.gestionbdd import GestionBdd
 
 class Root(Tk):
 	def __init__(self,mode_os):
 		self.mode_os=mode_os
 		Tk.__init__(self)
+		
+		if mode_os=="tablette":
+			from interface.interfacetablette import InterfaceTablette
+			self.interface=InterfaceTablette(self)
+			self.interface.grid()
+			
+		self.gestionbdd=GestionBdd(self)
+			
 		
 		
 if __name__=="__main__":
---
+++ src/gestionbdd.py
@@ -0,0 +1,5 @@
+import sqlite3
+
+class GestionBdd():
+	def __init__(self,boss):
+		self.boss=boss
